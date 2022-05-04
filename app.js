c33c98f1b3762f430696eb6a53518af86807af1e
ThanhLH <thanhlh@hybrid-technologies.vn>
2022-05-04 11:31:18 +0700

    check

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser")
 
 const app = express()
 app.get('/', (req, res) => {
